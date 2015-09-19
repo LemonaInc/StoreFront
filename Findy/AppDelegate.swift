ca864c13bebb4f6a89b8877766c6100d5980188b
LemonaInc <jaxonstevens@lemonainc.com>
2015-09-19 19:37:28 -0400

    parse

--- Findy/AppDelegate.swift
+++ Findy/AppDelegate.swift
@@ -7,10 +7,36 @@
 //
 
 import UIKit
+import Parse
+import Bolts
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+        
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("nPWhxpjQHrrG6WSJGKTeGHodbkNwIfq6G8xTsWnB",
+            clientKey: "oxOkyvuCZv6ve86z3xJJhR8Y9UqZ6xh2rP64MlM8")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
+        // ...
+    }
+    
+    // ...
+    
+    
+    
+    
+    
+    
     var window: UIWindow?
     let kUserHasAlreadyLoggedIn = "UserHasAlreadyLoggedIn"
 
